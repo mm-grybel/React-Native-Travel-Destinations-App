2ad9cf4f9dab05386615baf92d5af8dc142d0379
mgrybel <monika.grybel@hotmail.com>
2021-02-08 16:04:03 +0000

    Added storing images on the FileSystem

--- store/actions/destinations.js
+++ store/actions/destinations.js
@@ -1,11 +1,29 @@
+import * as FileSystem from 'expo-file-system';
+
 export const ADD_DESTINATION = 'ADD_DESTINATION';
 
 export const addDestination = (name, image) => {
-    return {
-        type: ADD_DESTINATION,
-        destinationData: {
-            name: name,
-            image: image
+    return async dispatch => {
+        const fileName = image.split('/').pop(); // the last part of the path
+        const newPath = FileSystem.documentDirectory + fileName;
+
+        try {
+            await FileSystem.moveAsync({
+                from: image,
+                to: newPath
+            });
+        } catch (err) {
+            // TODO: store errors on an analytics server
+            console.log(err);
+            throw err;
         }
+
+        dispatch({
+            type: ADD_DESTINATION,
+            destinationData: {
+                name: name,
+                image: image
+            }
+        });
     };
 };
