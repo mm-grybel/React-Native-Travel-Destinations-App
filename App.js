b4802b71c5ca235326e0ca0cac17ed1af9f0038a
mgrybel <monika.grybel@hotmail.com>
2021-03-24 20:47:41 +0000

    Update App.js

--- App.js
+++ App.js
@@ -8,24 +8,24 @@ import destinationsReducer from './store/reducers/destinations';
 import { init } from './helpers/db';
 
 init()
-  .then(() => {
-    console.log('Initialized the database.');
-  })
-  .catch(err => {
-    console.log('Initializing the database failed.');
-    console.log(err);
-  });
+    .then(() => {
+        console.log('Initialized the database.');
+    })
+    .catch(err => {
+        console.log('Initializing the database failed.');
+        console.log(err);
+    });
 
 const rootReducer = combineReducers({
-  destinations: destinationsReducer
+    destinations: destinationsReducer
 });
 
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <DestinationsNavigator />
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <DestinationsNavigator />
+        </Provider>
+    );
 }
